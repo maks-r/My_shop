ceaa8f46f183e024a8556239cb09b96164f08707
maks-r <maks.rus.in@mail.ru>
2022-04-15 17:29:20 +0300

    Lesson_5 is done

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Product, ProductCategory
 
 
 MENU_LINKS = [
-    {"url": "main", "name": "домой"},
-    {"url": "mainapp:products", "name": "продукты"},
-    {"url": "contact", "name": "контакты"},
+    {"url": "main", "active": ["main"], "name": "домой"},
+    {"url": "products:all", "active": ["products:all", "products.category"], "name": "продукты"},
+    {"url": "contact", "active": ["contact"], "name": "контакты"},
 ]
 
 
@@ -14,18 +14,23 @@ def index(request):
     return render(
         request,
         "mainapp/index.html",
-        context={"title": "Главная", "menu_links": MENU_LINKS, "products": products},
+        context={
+            "title": "Главная", 
+            "menu_links": MENU_LINKS, 
+            "products": products
+        },
     )
 
 
 def products(request):
-    categories = ProductCategory.objects.all()[:4]
+    categories = ProductCategory.objects.all()
+    products = Product.objects.all()[:4]
     return render(
         request,
         "mainapp/products.html",
         context={
             "title": "Продукты",
-            "products": [],
+            "products": products,
             "menu_links": MENU_LINKS,
             "categories": categories,
         },
@@ -33,7 +38,19 @@ def products(request):
 
 
 def category(request, pk):
-    return products(request)
+    categories = ProductCategory.objects.all()
+    category = get_object_or_404(ProductCategory, id=pk)
+    products = Product.objects.filter(category=category)
+    return render(
+        request,
+        "mainapp/products.html",
+        context={
+            "title": "Продукты",
+            "products": products,
+            "menu_links": MENU_LINKS,
+            "categories": categories,
+        },
+    )
 
 
 def contact(request):
